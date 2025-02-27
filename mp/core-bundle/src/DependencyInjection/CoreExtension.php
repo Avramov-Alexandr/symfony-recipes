57cdd33257b6927c14547a7fcad702ccceb38e49
Martin Muller <pr5@myworkplanner.net>
2025-02-27 11:09:03 +0000

    New day 27  commit

--- mp/core-bundle/src/DependencyInjection/CoreExtension.php
+++ mp/core-bundle/src/DependencyInjection/CoreExtension.php
@@ -6,8 +6,10 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-class CoreExtension extends Extension
+
+class CoreExtension extends Extension implements ExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
