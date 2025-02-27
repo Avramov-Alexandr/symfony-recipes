e01492790b050a74d00a25dd7d23afc4da25c99f
Martin Muller <pr5@myworkplanner.net>
2025-02-27 10:34:54 +0000

    New day 24  commit

--- mp/core-bundle/src/DependencyInjection/Configuration.php
+++ mp/core-bundle/src/DependencyInjection/Configuration.php
@@ -6,6 +6,10 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('core_bundle');
