b89b88eabe688cba77b0c6e45586d6219343608a
Martin Muller <pr5@myworkplanner.net>
2025-03-03 08:12:56 +0000

    New day 66  commit

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,17 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use MP\CoreBundle\Routing\CoreBundleLoader;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        parent::configureRoutes($routes);
+
+        $routes->import(CoreBundleLoader::class)
+            ->prefix('/core');
+    }
 }
