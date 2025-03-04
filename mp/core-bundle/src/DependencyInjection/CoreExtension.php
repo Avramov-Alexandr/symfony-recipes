336fec68f8de3bb51b11235f2f84bd796a83e089
Martin Muller <pr5@myworkplanner.net>
2025-03-04 07:45:54 +0000

    New day 98  commit

--- mp/core-bundle/src/DependencyInjection/CoreExtension.php
+++ mp/core-bundle/src/DependencyInjection/CoreExtension.php
@@ -16,7 +16,7 @@ class CoreExtension extends Extension implements ExtensionInterface
 //        $configuration = new Configuration();
 //        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/mp/core-bundle/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
         //$loader->load('routes.yaml');
 
