10fe02a8ae28fc692e33b1566a89ab7b1944c9fe
Martin Muller <pr5@myworkplanner.net>
2025-03-07 07:19:29 +0000

    New day 105  commit

---
+++ mp/core-bundle/src/Routing/RouteLoader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MP\CoreBundle\Routing;
+
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Routing\RouteCollection;
+
+class RouteLoader extends Loader
+{
+    private bool $isLoaded = false;
+
+    public function load($resource, ?string $type = null): RouteCollection
+    {
+        $routes = new RouteCollection();
+
+        if (!$this->isLoaded) {
+            $resource = '@CoreBundle/config/routes.yaml';
+            $type = 'yaml';
+
+            $importedRoutes = $this->import($resource, $type);
+
+            $routes->addCollection($importedRoutes);
+
+            $this->isLoaded = true;
+        }
+
+        return $routes;
+    }
+
+    public function supports(mixed $resource, ?string $type = null): bool
+    {
+        return 'attribute' === $type || 'core_bundle' === $type;
+    }
+}
